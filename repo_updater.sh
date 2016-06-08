4f9639e25d01893c2a0269c9a9380ee7b7f60655
kevin xu <cming.xu@gmail.com>
2016-06-08 18:38:38 +0800

    [MOD] update

--- repo_updater.sh
+++ repo_updater.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-#HARBOR_URL=http://forward.dataman-inc.com/
-HARBOR_URL=https://forward.shurenyun.com/
-#HARBOR_URL=127.0.0.1:8080
-USERNAME=admin
-TOKEN=2081a290c1854787b6d1cb784f0b0dc3
-#TOKEN=001122
+HARBOR_URL=http://devforward.dataman-inc.net/
+TOKEN=b022bb90d84d420796e7aa7942906189
 PASSWORD=Dataman1234
 
 PROJECT_NAME=library
@@ -16,7 +12,7 @@ update_repo_category()
   echo ""
   echo " ====  UPDATING $1's category to $2 ===="
   echo ""
-  curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"category\": \"$2\" } "
+  curl -H "Authorization: $TOKEN" -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"category\": \"$2\" } "
 }
 
 # 更新描述
@@ -25,7 +21,7 @@ update_repo_description()
   echo ""
   echo " ====  UPDATING $1's description to $2 ===="
   echo ""
-  curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD -X PUT -k -H "Content-Type: application/json"  $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"description\": \"$2\" } "
+  curl -H "Authorization: $TOKEN" -X PUT -k -H "Content-Type: application/json"  $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"description\": \"$2\" } "
 }
 
 # 更新docker compose
@@ -34,8 +30,8 @@ update_repo_docker_compose()
   echo ""
   echo " ====  UPDATING $1's docker compose to $2 ===="
   echo ""
-  echo curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD   -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"dockerCompose\": \"`REPO=$1 ruby -rbase64 -e 'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/docker_compose.yml\")).gsub(\"\\n\", "")'`\" }"
-  curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD  -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"dockerCompose\": \"`REPO=$1 ruby -rbase64 -e  'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/docker_compose.yml\")).gsub(\"\\n\",\"\")'`\" }"
+  echo curl -H "Authorization: $TOKEN"  -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"dockerCompose\": \"`REPO=$1 ruby -rbase64 -e 'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/docker_compose.yml\")).gsub(\"\\n\", "")'`\" }"
+  curl -H "Authorization: $TOKEN" -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"dockerCompose\": \"`REPO=$1 ruby -rbase64 -e  'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/docker_compose.yml\")).gsub(\"\\n\",\"\")'`\" }"
 }
 
 # 更新catalog
@@ -44,8 +40,8 @@ update_repo_catalog()
   echo ""
   echo " ====  UPDATING $1's catalog to $2 ===="
   echo ""
-  echo curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD   -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"catalog\": \"`REPO=$1 ruby -rbase64 -e 'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/catalog.yml\")).gsub(\"\\n\", "")'`\" } "
-  curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD  -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"catalog\": \"`REPO=$1 ruby -rbase64 -e  'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/catalog.yml\")).gsub(\"\\n\",\"\")'`\" } "
+  echo curl -H "Authorization: $TOKEN"    -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"catalog\": \"`REPO=$1 ruby -rbase64 -e 'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/catalog.yml\")).gsub(\"\\n\", "")'`\" } "
+  curl -H "Authorization: $TOKEN" -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"catalog\": \"`REPO=$1 ruby -rbase64 -e  'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/catalog.yml\")).gsub(\"\\n\",\"\")'`\" } "
 
 }
 
@@ -55,8 +51,8 @@ update_repo_marathon_config()
   echo ""
   echo " ====  UPDATING $1's marathon_config to $2 ===="
   echo ""
-  echo curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"marathonConfig\": \"`REPO=$1 ruby -rbase64 -e 'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/marathon_config.yml\")).gsub(\"\\n\", "")'`\" } "
-  curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD  -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"marathonConfig\": \"`REPO=$1 ruby -rbase64 -e  'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/marathon_config.yml\")).gsub(\"\\n\",\"\")'`\" } "
+  echo curl -H "Authorization: $TOKEN" -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"marathonConfig\": \"`REPO=$1 ruby -rbase64 -e 'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/marathon_config.yml\")).gsub(\"\\n\", "")'`\" } "
+  curl -H "Authorization: $TOKEN"  -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"marathonConfig\": \"`REPO=$1 ruby -rbase64 -e  'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/marathon_config.yml\")).gsub(\"\\n\",\"\")'`\" } "
 }
 
 
@@ -66,8 +62,8 @@ update_repo_readme()
   echo ""
   echo " ====  UPDATING $1's readme to $2 ===="
   echo ""
-  echo curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"readme\": \"`REPO=$1 ruby -rbase64 -e 'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/README.md\")).gsub(\"\\n\", "")'`\" } "
-  curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD   -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"readme\": \"`REPO=$1 ruby -rbase64 -e  'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/README.md\")).gsub(\"\\n\",\"\")'`\" } "
+  echo curl -H "Authorization: $TOKEN"  -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"readme\": \"`REPO=$1 ruby -rbase64 -e 'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/README.md\")).gsub(\"\\n\", "")'`\" } "
+  curl -H "Authorization: $TOKEN" -X PUT -k -H "Content-Type: application/json" $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"readme\": \"`REPO=$1 ruby -rbase64 -e  'puts Base64.encode64(File.read(Dir.pwd + \"/library/#{ENV[\"REPO\"]}/README.md\")).gsub(\"\\n\",\"\")'`\" } "
 }
 # 更新状态是否可见
 update_repo_is_public()
@@ -75,8 +71,8 @@ update_repo_is_public()
   echo ""
   echo " ====  UPDATING $1's publicity to $2 ===="
   echo ""
-  echo curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD -X PUT -k -H "Content-Type: application/json"  $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"isPublic\":  $2} "
-  curl -H "Authorization: $TOKEN" -u $USERNAME:$PASSWORD -X PUT -k -H "Content-Type: application/json"  $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"isPublic\":  $2} "
+  echo curl -H "Authorization: $TOKEN" -X PUT -k -H "Content-Type: application/json"  $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"isPublic\":  $2} "
+  curl -H "Authorization: $TOKEN" -X PUT -k -H "Content-Type: application/json"  $HARBOR_URL/api/v3/repositories/$PROJECT_NAME/$1 -d " { \"isPublic\":  $2} "
 }
 
 for repo in `ls library`; do
